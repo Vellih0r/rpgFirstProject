7ccf8cb6d3681e07fed680afa2fc9da1f8c60e41
Misha2502 <yegrashich@gmail.com>
2024-07-30 22:37:17 +0300

    класс person с полем name

--- our game.py
+++ our game.py
@@ -1,5 +1,10 @@
 class Person:
-    def __init__(self):
-        print("Создан Миша")
+    def __init__(self, name):
+        self. name = name
+        if self.name == 'Misha':
+            print("Создан Миша")
+        elif self.name == 'Artyom':
+            print("Создан Артём")
 
-misha = Person()
+misha = Person("Misha")
+artyom = Person("Artyom")
