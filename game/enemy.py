bf10e6339fde18bf086685123de05d599697dfd2
Senku-qq <maksfloopy@gmail.com>
2024-08-01 17:16:15 +0200

    added a fishing mechanic

---
+++ game/enemy.py
@@ -0,0 +1,8 @@
+class Enemy():
+    def __init__(self,name):
+        self.name = name 
+        #added enemy stats
+        self.health = 20
+        self.armor = 1 
+        self.physic_damage = 15
+        
