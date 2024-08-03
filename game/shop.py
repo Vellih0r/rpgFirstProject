aabd0f026e3d283d8fa9d96ef4ca5779607882cc
Senku-qq <maksfloopy@gmail.com>
2024-08-03 16:06:16 +0200

    shop fixed

--- game/shop.py
+++ game/shop.py
@@ -1,5 +1,5 @@
 import player
-import random
+from random import randint
 import welcomeclass
 from welcomeclass import decorator
 
@@ -13,18 +13,14 @@ def shopfunc(nickname, balance, items):
         Вітаємо в магазині, {nickname}!
       Твій баланс: {balance}
 ''')
-    global pricerand
-    pricerand = random.randint(10,50)
-    global pricerand1
-    pricerand1 = random.randint(10,50)
-    global pricerand2
-    pricerand2 = random.randint(10,50)
-    global pricerand3
-    pricerand3 = random.randint(10,50)
-    towars = {'Клінок': pricerand,'Щит': pricerand1,'Дрібничка': pricerand2, 'Вудочка': pricerand3}
-    for key, val in towars.items():
+    global dict 
+    dict = {}
+    for i in items:
+        pricerand = randint(10,40)
+        dict[i] = pricerand
+    
+    for key, val in dict.items():
         print(f"Товар {key} коштує {val}\n")
-
 
 # гравець вибирає та купує предмет
 def buy(character):
@@ -35,35 +31,46 @@ def buy(character):
     buying = buying.title()
 
     if buying == 'Клінок':
-        if character.balance < pricerand:
+        if character.balance < dict['Клінок']: 
             print("Вам не вистачає грошей.")
-        character.balance -= pricerand
-        print(f"Ви купували Клінок!")
-        character.add_item('Клінок')
-        character.update_stats()
-        back = input("Щоб повернутись назад, натисніть Enter\n")
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
+        else:
+            character.balance -= dict['Клінок']
+            print("Ви купували Клінок!")
+            character.add_item('Клінок')
+            character.update_stats()
+            back = input("Щоб повернутись назад, натисніть Enter\n")
     elif buying == 'Щит':
-        character.balance -= pricerand1
-        if character.balance < pricerand1:
+        if character.balance <  dict['Щит']:  
             print("Вам не вистачає грошей.")
-        print(f"Ви купували Щит!")
-        character.add_item('Щит')
-        character.update_stats()
-        back = input("Щоб повернутись назад, натисніть ENTER\n")
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
+        else:
+            character.balance -=  dict['Щит']
+            print("Ви купували Щит!")
+            character.add_item('Щит')
+            character.update_stats()
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
     elif buying == 'Дрібничка':
-        character.balance -= pricerand2
-        if character.balance < pricerand2: print("Вам не вистачає грошей.")
-        print(f"Ви купували Дрібничку!")
-        character.add_item('Дрібничка')
-        character.update_stats()
-        back = input("Щоб повернутись назад, натисніть ENTER\n")
+        if character.balance < dict['Дрібничка']: 
+            print("Вам не вистачає грошей.")
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
+        else: 
+            character.balance -= dict['Дрібничка']
+            print("Ви купували Дрібничку!")
+            character.add_item('Дрібничка')
+            character.update_stats()
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
     elif buying == 'Вудочка':
-        character.balance -= pricerand2
-        if character.balance < pricerand2: print("Вам не вистачає грошей.")
-        print(f"Ви купували Вудочку!")
-        character.add_item('Вудочка')
-        character.update_stats()
-        back = input("Щоб повернутись назад, натисніть ENTER\n")
+        character.balance -= dict['Вудочка']
+        if character.balance < dict['Вудочка']: 
+            print("Вам не вистачає грошей.")
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
+        else:
+            character.balance -= dict['Вудочка']
+            print("Ви купували Вудочку!")
+            character.add_item('Вудочка')
+            character.update_stats()
+            back = input("Щоб повернутись назад, натисніть ENTER\n")
     if back == '':
         welcomeclass.welcome(character.name, character.balance)
     
