a5f398e041d972ac5b4365296160c9287346ed20
shaddoooww <ppupkin1337@gmail.com>
2024-08-06 18:25:02 +0300

    небольшие изменения

--- other_files/other_code.py
+++ other_files/other_code.py
@@ -4,9 +4,9 @@ import pyglet
 def language():
     music = pyglet.resource.media("Static (cut).mp3")
     music.play()
-    pyglet.app.run()
-    input(f'''Обери мову / Choose the language:\n
+    print(f'''Обери мову / Choose the language:\n
 >> english
 >> spanish\n''')
+    pyglet.app.run()
 
 language()
